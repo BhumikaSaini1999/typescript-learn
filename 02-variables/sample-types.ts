342a32cdae788bbd1d95e3ba81e9a4c7d6962e2e
BhumikaSaini1999 <sainibhumika1624@gmail.com>
2024-06-09 08:21:19 +0530

    typescript files added

---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,16 @@
+let found: boolean =true;
+let grade: number = 88.6;
+let firstName: string = "Anup";
+let lastName: string = 'Kumar';
+
+//let's break it
+// found=0;
+// grade="A";
+// firstName=false;
+
+console.log(found);
+console.log("The grade is: "+grade);
+console.log("Hi "+firstName+" "+lastName);
+
+//use templates and strings (using backticks)
+console.log(`Hello ${firstName} ${lastName}`);
---
+++ 04-classes/05-modules/Driver.ts
@@ -0,0 +1,5 @@
+import {Customer} from './Customer';
+
+let myCustomer = new Customer("Bhumika","Saini");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
